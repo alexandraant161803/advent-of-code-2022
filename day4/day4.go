9fdf293ad8b03abd53bdb1bfaa2442b620d8f66c
Alexandra Antgren <alexandra.antgren@northvolt.com>
2022-12-04 21:09:15 +0100

    day 4 part 1

---
+++ day4/day4.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+/*In how many assignment pairs does one range fully contain the other?*/
+
+func main() {
+	input, _ := os.Open("day4input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+
+	var sum int
+	for sc.Scan() {
+		var minPair1, maxPair1, minPair2, maxPair2 int
+		fmt.Sscanf(sc.Text(), "%v-%v,%v-%v", &minPair1, &maxPair1, &minPair2, &maxPair2)
+
+		if minPair1 <= minPair2 && maxPair1 >= maxPair2 || minPair2 <= minPair1 && maxPair2 >= maxPair1 {
+			sum += 1
+		}
+
+	}
+	fmt.Println(sum)
+}
