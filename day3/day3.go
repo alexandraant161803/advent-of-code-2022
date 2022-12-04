b44a27669130db94c1074c252135aca96c6d80e5
Alexandra Antgren <alexandra.antgren@northvolt.com>
2022-12-04 13:53:03 +0100

    day3

--- day3/day3.go
+++ day3/day3.go
@@ -1,36 +1,43 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 /*
 Info:
 Lowercase item types a through z have priorities 1 through 26.
 Uppercase item types A through Z have priorities 27 through 52.
+*/
+var priorities = map[string]int{
+	"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26,
+	"A": 27, "B": 28, "C": 29, "D": 30, "E": 31, "F": 32, "G": 33, "H": 34, "I": 35, "J": 36, "K": 37, "L": 38, "M": 39, "N": 40, "O": 41, "P": 42, "Q": 43, "R": 44, "S": 45, "T": 46, "U": 47, "V": 48, "W": 49, "X": 50, "Y": 51, "Z": 52}
 
+// instead of priorities mapping one could also use ASCII value mapping: https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+
+func main() {
+	input, err := os.ReadFile("day3input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	elfsItems := strings.Split(string(input), "\n")
+	part1(elfsItems)
+	part2(elfsItems)
+
+}
+
+/*
 Task:
 Find the item type that appears in both compartments of each rucksack.
 What is the sum of the priorities of those item types?
-
 */
-
-func main() {
-	// Read the file
-	input, _ := os.Open("day3input.txt")
-	defer input.Close()
-	sc := bufio.NewScanner(input)
-
-	priorities := map[string]int{
-		"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26,
-		"A": 27, "B": 28, "C": 29, "D": 30, "E": 31, "F": 32, "G": 33, "H": 34, "I": 35, "J": 36, "K": 37, "L": 38, "M": 39, "N": 40, "O": 41, "P": 42, "Q": 43, "R": 44, "S": 45, "T": 46, "U": 47, "V": 48, "W": 49, "X": 50, "Y": 51, "Z": 52}
-
+func part1(elfsItems []string) {
 	sum := 0
-	for sc.Scan() {
+	for _, compartments := range elfsItems {
 		itemTypes := map[string]int{}
-		compartments := sc.Text()
 		firstCompartment := compartments[:len(compartments)/2]
 		secondCompartment := compartments[len(compartments)/2:]
 
@@ -43,15 +50,59 @@ func main() {
 
 		for _, item := range secondCompartment {
 			item := string(item)
-			if occurences, ok := itemTypes[item]; ok {
-				if occurences >= 1 {
-					continue
-				}
+			if _, ok := itemTypes[item]; ok {
 				sum += priorities[item]
-				itemTypes[item] += 1
+				break
 
 			}
 		}
+
+	}
+
+	fmt.Println(sum)
+}
+
+/*
+
+Find the one item type that is common between all three Elves in each group.
+What is the sum of the priorities of those item types?
+*/
+
+func part2(elfsItems []string) {
+	sum := 0
+	i := 1
+	itemTypes := map[string]int{}
+	for _, elf := range elfsItems {
+		if i == 1 {
+			for _, item := range elf {
+				item := string(item)
+				if _, ok := itemTypes[item]; !ok {
+					itemTypes[item] = 1
+				}
+			}
+
+		} else if i == 2 {
+			for _, item := range elf {
+				item := string(item)
+				if _, ok := itemTypes[item]; ok {
+					itemTypes[item] = 2
+				}
+			}
+
+		} else {
+			i = 0
+			for _, item := range elf {
+				item := string(item)
+				if _, ok := itemTypes[item]; ok {
+					if itemTypes[item] == 2 {
+						sum += priorities[item]
+						break
+					}
+				}
+			}
+			itemTypes = map[string]int{}
+		}
+		i++
 
 	}
 
